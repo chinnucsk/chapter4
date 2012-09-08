dd6646c4fc48dae14fa4559a077a40030eb95ac9
Tyralion <piliaiev@gmail.com>
2012-09-08 19:13:46 +0600

    Простой echo-сервер.

---
+++ echo.erl
@@ -0,0 +1,25 @@
+-module(echo).
+-export([go/0, loop/0]).
+
+go() ->
+  
+  Pid = spawn(echo, loop, []),
+  Pid ! { self(), hello },
+
+  receive
+    { _Pid, Msg } -> io:format("~w~n", [Msg])
+  end,
+
+  Pid ! stop.
+
+loop() ->
+  
+  receive
+    
+    { From, Msg } -> 
+      From ! { self(), Msg },
+      loop();
+
+    stop -> true
+
+  end.  
